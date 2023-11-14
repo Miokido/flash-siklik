5e36e86abfc5a58fbbc72e0a6ba0b49e854b09a6
trist <tristanbellan76@gmail.com>
2023-11-14 15:36:08 +0100

    V2 test de vehicule

--- tests/api/test_flash_siklik.py
+++ tests/api/test_flash_siklik.py
@@ -1,39 +1,40 @@
-import unittest
+import pytest
 
 from src.api.fs.Vehicle import Vehicle
 
 
-class TestFlashSiklikApi(unittest.TestCase):
+class TestFlashSiklikApi(object):
     """
         test de la classe vehicule
     """
 
     def testVehicule(self):
         vehiculeTest = Vehicle(1, 1, "test", "test", "test", 1)
-        self.assertTrue(vehiculeTest.turn(-1),False)
-        self.assertTrue(vehiculeTest.turn(0),True)
-        self.assertTrue(vehiculeTest.turn(1),True)
-        self.assertTrue(vehiculeTest.turn(2),True)
-        self.assertTrue(vehiculeTest.turn(3),True)
-        self.assertTrue(vehiculeTest.turn(4),False)
 
-        self.assertTrue(vehiculeTest.boost(True),None)
-        self.assertTrue(vehiculeTest.boost(False),None)
+        assert vehiculeTest.turn(-1) == False
+        assert vehiculeTest.turn(0) == True
+        assert vehiculeTest.turn(1) == True
+        assert vehiculeTest.turn(2) == True
+        assert vehiculeTest.turn(3) == True
+        assert vehiculeTest.turn(4) == False
 
-        self.assertTrue(vehiculeTest.useItem(-1),False)
-        self.assertTrue(vehiculeTest.useItem(0),True)
-        self.assertTrue(vehiculeTest.useItem(1),True)
-        self.assertTrue(vehiculeTest.useItem(2),True)
-        self.assertTrue(vehiculeTest.useItem(3),True)
-        self.assertTrue(vehiculeTest.useItem(4),False)
+        assert vehiculeTest.boost(True) is None
+        assert vehiculeTest.boost(False) is None
 
-        self.assertTrue(vehiculeTest.update(),None)
+        assert vehiculeTest.useItem(-1) == False
+        assert vehiculeTest.useItem(0) == True
+        assert vehiculeTest.useItem(1) == True
+        assert vehiculeTest.useItem(2) == True
+        assert vehiculeTest.useItem(3) == True
+        assert vehiculeTest.useItem(4) == False
 
-        self.assertTrue(vehiculeTest.getItems(),None)
-        self.assertTrue(vehiculeTest.getUsedItems(),None)
-        self.assertTrue(vehiculeTest.hasBoost(),False)
-        self.assertTrue(vehiculeTest.hasSuperBoost(),False)
-        self.assertTrue(vehiculeTest.isGhost(),False)
-        self.assertTrue(vehiculeTest.hasShield(),False)
-        self.assertTrue(vehiculeTest.getX(),0)
-        self.assertTrue(vehiculeTest.getY(),0)
+        assert vehiculeTest.update() is None
+
+        assert vehiculeTest.getItems() is None
+        assert vehiculeTest.getUsedItems() is None
+        assert vehiculeTest.hasBoost() == False
+        assert vehiculeTest.hasSuperBoost() == False
+        assert vehiculeTest.isGhost() == False
+        assert vehiculeTest.hasShield() == False
+        assert vehiculeTest.getX() == 0
+        assert vehiculeTest.getY() == 0
