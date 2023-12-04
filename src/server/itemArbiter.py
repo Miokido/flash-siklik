520ef6f3e361e437d209b8ea389693db648025c4
Baptiste CHERUEL <baptiste.cheruel@etu.unicaen.fr>
2023-12-04 09:15:17 +0100

    Item arbiter *murlock noises*

--- src/server/itemArbiter.py
+++ src/server/itemArbiter.py
@@ -1,23 +1,18 @@
 from j2l.pytactx.agent import Agent
 from ..api.fs.Vehicle import Vehicle
+from random import *
 
 
 class IItemArbiter:
-    def retrieveItem(self, agent: Vehicle, item: str) -> None:
+    def spawnItem(self, game_map: [[], []]) -> None:
+        """
+        Add an item on the map
+        """
+        ...
+
+    def retrieveItem(self, agent: Agent, item: str, game_map: [[], []]) -> None:
         """
         Add the item to the list of the agent
-        """
-        ...
-
-    def useItem(self, agent: Vehicle, item: str) -> None:
-        """
-        Use and remove the item from the agent list
-        """
-        ...
-
-    def spawnItem(self) -> None:
-        """
-        Spawn an item at regular interval
         """
         ...
 
@@ -26,9 +21,16 @@ class ItemArbiter(IItemArbiter):
     def __init__(self, agent: Agent) -> None:
         self.__pytactxAgent = agent
 
-    def useItem(self, agent: Vehicle, item: str) -> None:
-        if item in agent.getItems():
-            agent.getItems().remove(item)
+    def spawnItem(self, game_map: [[], []]) -> None:
+        x = randint(0, 50)
+        y = randint(0, 50)
+        if game_map[x][y] == 0:
+           game_map[x][y] = 3
+        else: self.spawnItem(game_map)
 
-    def spawnItem(self) -> None:
-        item = Agent()
+    def retrieveItem(self, agent: Agent, item: str, game_map: [[], []]) -> None:
+        x = agent.getX()
+        y = agent.getY()
+        if game_map[x][y] == 3:
+            game_map[x][y] = 0
+            agent.addItem(item)
